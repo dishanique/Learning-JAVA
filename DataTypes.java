3408045c6325b95dd170725ab8a41118a2aa852d
Di'Shanique Al-Malik <dishanique.almalik@gmail.com>
2016-09-29 13:31:22 -0400

    Create DataTypes.java
    doc: DataTypes.jave
    feat: simple Data types using java

---
+++ DataTypes.java
@@ -0,0 +1,27 @@
+public class DataTypes {
+	public static void main(String[] args) {
+
+		System.out.println(7);
+
+	}
+}
+
+// DataTypeB
+
+public class DataTypesB {
+	public static void main(String[] args) {
+
+		System.out.println(true);
+
+	}
+}
+
+// DataTypeC
+
+public class DataTypesC {
+	public static void main(String[] args) {
+
+		System.out.println('D');
+
+	}
+}
